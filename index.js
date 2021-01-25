2bc1df4d4169e39f005eb97fbf1c0a635be724b0
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-25 18:27:01 -0400

    Added ability to query movie database

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hi there!');
+async function fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '191dfc62',
+            s: searchTerm
+        }
+    });
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+const onInput = debounce((e) => {
+    //fetchData(e.target.value);
+    console.log(e.target.value);
+}, 500);
+
+input.addEventListener('input', onInput);
