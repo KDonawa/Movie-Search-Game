7dfd1afcc8bb133e57b8fb73ace2b81bcfa1af7b
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-26 15:31:52 -0400

    Added CSS styling and dropdown menu for movie search

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 async function fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get("http://www.omdbapi.com/", {
         params: {
-            apikey: '191dfc62',
-            s: searchTerm
-        }
+            apikey: "191dfc62",
+            s: searchTerm,
+        },
     });
-    console.log(response.data);
-};
+    return response.data.Error ? [] : response.data.Search;
+}
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML =`
+    <label><b>Search For a Movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
-const onInput = debounce((e) => {
-    //fetchData(e.target.value);
-    console.log(e.target.value);
-}, 500);
+async function onInput(e) {
+    const movies = await fetchData(e.target.value);
+    
+    dropdown.classList.add('is-active');
 
-input.addEventListener('input', onInput);
+    for (const movie of movies) {
+        const option = document.createElement('a');
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+            <img src="${movie.Poster}">
+            ${movie.Title}
+        `;
+
+        resultsWrapper.appendChild(option);
+    }
+}
+
+input.addEventListener("input", debounce(onInput, 1000));
