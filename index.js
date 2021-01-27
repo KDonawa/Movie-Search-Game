9db5a4f3c5e97b692636f1b6d7d80af48236d240
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-27 12:04:37 -0400

    Added logic to compare movie selections

--- index.js
+++ index.js
@@ -1,70 +1,103 @@
 const dropdownConfig = {
-    optionDisplay: (movie) => {
+    optionDisplay: (movieData) => {
         return `
-            <img src="${movie.Poster === 'N/A' ? '' : movie.Poster}">
-            ${movie.Title} (${movie.Year})
+            <img src="${movieData.Poster === 'N/A' ? '' : movieData.Poster}">
+            ${movieData.Title} (${movieData.Year})
         `;
     },
-    inputValue: movie => movie.Title,
+    inputValue: movieData => movieData.Title,
     fetchOptions: async (searchTerm) => await fetchData(searchTerm)
 };
 
+let leftMovieDetails;
+let rightMovieDetails;
 createDropdown({
-    root: document.querySelector('#left-search'),
-    onOptionSelected: async (movie) => {
-        await onOptionSelectedHelper(movie, document.querySelector('#left-details'))
+    root: document.querySelector('#search1'),
+    onOptionSelected: async (movieData) => {
+        leftMovieDetails = await displayMovieDetails(movieData, document.querySelector('#details1'));
+        compareMovies();
     },
     ...dropdownConfig
 });
 createDropdown({
-    root: document.querySelector('#right-search'),
-    onOptionSelected: async (movie) => {
-        await onOptionSelectedHelper(movie, document.querySelector('#right-details'))
+    root: document.querySelector('#search2'),
+    onOptionSelected: async (movieData) => {
+        rightMovieDetails = await displayMovieDetails(movieData, document.querySelector('#details2'));
+        compareMovies();
     },
     ...dropdownConfig
 });
 
-async function onOptionSelectedHelper(movie, target) {
-    const movieData = await fetchDataById(movie.imdbID);
-    if (movieData) {
+async function displayMovieDetails(movieData, displayLocation) {
+    const movieDetails = await fetchDataById(movieData.imdbID);
+    if (movieDetails) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        target.innerHTML = generateMovieTemplate(movieData);
+        displayLocation.innerHTML = generateDisplayTemplate(movieDetails);
+    }
+    return movieDetails;
+};
+function compareMovies(){
+    if(leftMovieDetails && rightMovieDetails){
+        const leftMovieStats = document.querySelectorAll('#details1 .notification');
+        const rightMovieStats = document.querySelectorAll('#details2 .notification');
+        for (let i = 0; i < leftMovieStats.length; i++) {
+            const leftStat = parseInt(leftMovieStats[i].dataset.value);
+            const rightStat = parseInt(rightMovieStats[i].dataset.value);
+            if(leftStat < rightStat){
+                leftMovieStats[i].classList.remove('is-primary');
+                leftMovieStats[i].classList.add('is-warning');
+            }
+            else if(leftStat > rightStat){
+                rightMovieStats[i].classList.remove('is-primary');
+                rightMovieStats[i].classList.add('is-warning');
+            }            
+        }
     }
 };
-function generateMovieTemplate(movieData) {
+function generateDisplayTemplate(movieDetails) {
+    const boxOffice = parseInt(movieDetails.BoxOffice.replace(/\$|,/g,""));
+    const metascore = parseInt(movieDetails.Metascore);
+    const rating = parseFloat(movieDetails.imdbRating);
+    const votes = parseInt(movieDetails.imdbVotes.replace(/,/g,""));
+    const awards = movieDetails.Awards
+        .split(' ')
+        .filter(x => parseInt(x))
+        .map(x => parseInt(x))
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
     return `
         <article class="media">
             <figure class="media-left">
                 <p class="image">
-                    <img src="${movieData.Poster}">
+                    <img src="${movieDetails.Poster}">
                 </p>
             </figure>
             <div class="media-content">
                 <div class="content">
-                    <h1>${movieData.Title}</h1>
-                    <h4>${movieData.Genre}</h4>
-                    <p>${movieData.Plot}</p>
+                    <h1>${movieDetails.Title}</h1>
+                    <h4>${movieDetails.Genre}</h4>
+                    <p>${movieDetails.Plot}</p>
                 </div>
             </div>
         </article>
-        <article class="notification is-primary">
-            <p class="title">${movieData.Awards}</p>
+        <article data-value=${awards} class="notification is-primary">
+            <p class="title">${movieDetails.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-primary">
-            <p class="title">${movieData.BoxOffice}</p>
+        <article data-value=${boxOffice} class="notification is-primary">
+            <p class="title">${movieDetails.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-primary">
-            <p class="title">${movieData.Metascore}</p>
+        <article data-value=${metascore} class="notification is-primary">
+            <p class="title">${movieDetails.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-primary">
-            <p class="title">${movieData.imdbRating}</p>
+        <article data-value=${rating} class="notification is-primary">
+            <p class="title">${movieDetails.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-primary">
-            <p class="title">${movieData.imdbVotes}</p>
+        <article data-value=${votes} class="notification is-primary">
+            <p class="title">${movieDetails.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
     `;
